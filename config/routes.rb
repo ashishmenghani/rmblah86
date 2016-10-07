9d6da3e26848e76c159053630bbe5b420c4a394d
Ashish Menghani <ashish.men@gmail.com>
2016-10-07 22:12:14 +0530

    Versioning changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
   Rails.application.routes.draw do
+  scope module: :v1, constraints: ApiConstraint.new(version: 1) do
   resources :open_lists
   resources :broadcast_lists, param: :list_id do
       resources :list_members
@@ -23,4 +24,8 @@
   
   post 'authenticate', to: 'authentication#authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+  scope module: :v2, constraints: ApiConstraint.new(version: 2) do
+    resources :firms  
+  end
 end
